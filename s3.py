f21d46980442ffbcdb4600c59559a48eb4d31004
Jared Hendrix <jaredhendrix2014@gmail.com>
2023-03-16 23:35:23 -0400

    Update 0.1.0

--- s3.py
+++ s3.py
@@ -1,197 +1,126 @@
-import os
-import logging
-import boto3
-import botocore
-import platform
+import mydefs
 import time
-from botocore.exceptions import ClientError
 
-s3 = boto3.resource('s3')
-global buckets_list
-buckets_list = []
-file_list = []
-global counter 
-counter = 0
-region = "us-east-2"
-
-for buckets in s3.buckets.all():
-    buckets_list.append(buckets.name)
-
-def upload_file(file_name, bucket, object_name=None):
-    # If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = os.path.basename(file_name)
-
-    # Upload the file
-    s3_client = boto3.client('s3')
-    try:
-        response = s3_client.upload_file(file_name, bucket, object_name)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
-
-def create_bucket(bucket_name):
-    # Create bucket
-    try:
-        if region is None:
-            s3_client = boto3.client('s3')
-            s3_client.create_bucket(Bucket=bucket_name)
-            print("The bucket was created!")
-        else:
-            s3_client = boto3.client('s3', region_name=region)
-            location = {'LocationConstraint': region}
-            s3_client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=location)
-            print("The bucket was created!")
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
-    menu_options()
-
-def delete_object(bucket, objectKey):
-    s3.Object(bucket, objectKey).delete()
-    print("Removing: ", objectKey)
-
-def delete_bucket(bucket):
-    counter = 0
-    for item in bucket.objects.all():
-        counter += 1
-    print(counter)
-    if counter >= 1:
-        print("Bucket is not empty, deleting all files.")
-        for item in bucket.objects.all():
-            bucket.Object(item.key).delete()
-            print("Deleting: ", item.key)
-        print("All items have been removed, now deleting bucket.")
-    bucket.delete()
-    print("Bucket deleted!")
-
-def download_file(bucket, objectkey):
-    try:
-        bucket.download_file(objectkey, objectkey)
-    except botocore.exceptions.ClientError as e:
-        if e.response['Error']['Code'] == "404":
-            print("The object does not exist.")
-        else:
-            raise
-
-def clear_console():
-    if platform.system() == "Windows":
-        os.system('cls')
-    else:
-        os.system()
-
-def menu_options():
-    buckets_list = []
-    for buckets in s3.buckets.all():
-        buckets_list.append(buckets.name)
-    counter = 0
-    print("Welcome to the basic S3 client!")
-    print(" 1. Create New Bucket \n 2. Upload New File \n 3. Delete an Existing File \n 4. Download File \n 5. Delete Bucket(Dangerous)")
-    menu_selection = input("What would you like to do?(1-5): ")
+def main_menu():
+    bucket_list = []
+    mydefs.clear_console()
+    print("Basic S3 Control Tool - Welcome, " + mydefs.os.getlogin() + ".")
+    print(" 1. Create Bucket \n 2. Delete Bucket(Dangerous) \n 3. Upload File \n 4. Download File \n 5. Delete File")
+    menu_selection = input("What would you like to do?(1-5): \n -> ")
     menu_selection = int(menu_selection)
-    #Create bucket option
     if menu_selection == 1:
-        bucket_name = input("What would you like the new bucket to be called? \n")
-        create_bucket(bucket_name)
-        print("Bucket: ", bucket_name, " has been created!")
-        time.sleep(3)
-        clear_console()
-        menu_options()
-
-    #upload file option
+        location = mydefs.select_location()
+        print(str(location))
+        bucket_name = input("What would you like the new bucket to be called? \n -> ")
+        mydefs.create_bucket(bucket_name, location)
     elif menu_selection == 2:
-        print("THIS IS HAVING ISSUES ON WINDOWS")
-        #checking number of buckets to figure out what to do        
-        if len(buckets_list) == 0:
-            bucketName = input("No buckets currently exist, please create one: ")
-            print("You chose: ", bucketName)
-            create_bucket(bucketName)
-            bucket = bucketName
-        else:
-            curcount = 0
-            for bucket in buckets_list:
-                print(curcount, " ", bucket)
-                curcount += 1
-            bucketName = input("Which bucket would you like to access?(Select Number): ")
-            bucketName = int(bucketName)
-
-        file_name = input("Enter the location of the file to upload: ")
-        print("You selected: ", os.path.basename(file_name))
-
-        print("Uploading your file, please wait...")
-        upload_file(file_name, bucket)
-        print("Upload Complete!")
-        time.sleep(3)
-        clear_console()
-        menu_options()
-    #Delete an object option
-    elif menu_selection == 3:
+        bucket_list = []
+        bucket_list = mydefs.load_buckets()
+        if len(bucket_list) == 0:
+            print("No buckets found.")
+            mydefs.clear_console()
+            main_menu()
         curcount = 0
-        for bucket in buckets_list:
-            print(curcount, " ", bucket)
-            curcount += 1
-        bucketName = input("Which bucket would you like to access?(Select Number): ")
-        bucketName = buckets_list[int(bucketName)]
-        bucket = s3.Bucket(bucketName)
-        curcount = 0
-        for s3_files in bucket.objects.all():
-            print(curcount, s3_files.key)
-            file_list.append(s3_files.key)
-            curcount += 1
-        if curcount == 0:
-            print("No files found in bucket:", bucket.name)
-            time.sleep(3)
-            clear_console()
-            menu_options()
-        objectkey = input("Which file would you like to delete?(Select a number): ")
-        delete_object(bucketName, file_list[int(objectkey)])
-        print("The file was deleted!")
-        time.sleep(3)
-        clear_console()
-        menu_options()
-    elif menu_selection == 4:
-        curcount = 0
-        for bucket in buckets_list:
-            print(curcount, " ", bucket)
-            curcount += 1
-        bucketName = input("Which bucket would you like to access?(Select Number): ")
-        bucketName = buckets_list[int(bucketName)]
-        bucket = s3.Bucket(bucketName)
-        curcount = 0
-        for s3_files in bucket.objects.all():
-            print(curcount, s3_files.key)
-            file_list.append(s3_files.key)
-            curcount += 1
-        if curcount == 0:
-            print("No files found in bucket:", bucket.name)
-            time.sleep(3)
-            clear_console()
-            menu_options()
-        objectkey = input("Which file would you like to download?(Select number): ")
-        download_file(bucket, file_list[int(objectkey)])
-        print("File: ", file_list[int(objectkey)], " has been downloaded!")
-        time.sleep(3)
-        clear_console()
-        menu_options()
-    elif menu_selection == 5:
-        curcount = 0
-        for bucket in buckets_list:
-            print(curcount, " ", bucket)
+        for bucket in bucket_list:
+            print(mydefs.get_location(bucket), "\n", curcount, " ", bucket)
             curcount += 1
         bucketName = input("Which bucket would you like to remove?(Select Number): ")
-        bucketName = buckets_list[int(bucketName)]
-        bucket = s3.Bucket(bucketName)
+        bucket = bucket_list[int(bucketName)]
+        bucket = mydefs.s3.Bucket(bucket)
         print("Running this command will permanently remove all files in the bucket and the bucket itself.")
         verify = input("Are you sure you want to remove this bucket and all of its contents? Type '"'permanently delete'"' \n")
         if verify.upper() == "PERMANENTLY DELETE":
-            delete_bucket(bucket)
+            mydefs.delete_bucket(bucket)
         else:
             print("Aborting...")
         time.sleep(3)
-        clear_console()
-        menu_options()
-    else:
-        exit()
-menu_options()
+        mydefs.clear_console()
+        main_menu()
+    elif menu_selection == 3:
+        bucket_list = mydefs.load_buckets()
+        if len(bucket_list) == 0:
+            print("No buckets found. Please make a new bucket first.")
+            time.sleep(3)
+            mydefs.clear_console()
+            main_menu()
+
+        #checking number of buckets to figure out what to do        
+        if len(bucket_list) == 0:
+            bucket = input("No buckets currently exist, please create one: \n -> ")
+            print("You chose: ", bucket)
+            mydefs.create_bucket(bucket)
+        else:
+            curcount = 0
+            for bucket in bucket_list:
+                print(mydefs.get_location(bucket), "\n", curcount, " ", bucket)
+                curcount += 1
+            bucket = input("Which bucket would you like to access?(Select Number): ")
+            bucket = bucket_list[int(bucket)]
+
+        file_name = input("Enter the location of the file to upload: ")
+        print("You selected: ", mydefs.os.path.basename(file_name))
+
+        print("Uploading your file, please wait...")
+        mydefs.upload_file(file_name, bucket)
+        print("Upload Complete!")
+        time.sleep(3)
+        mydefs.clear_console()
+        main_menu()
+    elif menu_selection == 4:
+        bucket_list = mydefs.load_buckets()
+        if len(bucket_list) == 0:
+            print("No data found, going back to main menu.")
+            time.sleep(3)
+            mydefs.clear_console()
+            main_menu()
+        curcount = 0
+        for bucket in bucket_list:
+            print(curcount, " ", bucket)
+            curcount += 1
+        bucketName = input("Which bucket would you like to access?(Select Number): ")
+        bucketName = bucket_list[int(bucketName)]
+        bucket = mydefs.s3.Bucket(bucketName)
+        curcount = 0
+        for s3_files in bucket.objects.all():
+            print(curcount, s3_files.key)
+            mydefs.file_list.append(s3_files.key)
+            curcount += 1
+        if curcount == 0:
+            print("No files found in bucket:", bucket.name)
+            time.sleep(3)
+            mydefs.clear_console()
+            main_menu
+        objectkey = input("Which file would you like to download?(Select number): ")
+        mydefs.download_file(bucket, mydefs.file_list[int(objectkey)])
+        print("File: ", mydefs.file_list[int(objectkey)], " has been downloaded!")
+        time.sleep(3)
+        mydefs.clear_console()
+        main_menu()
+    elif menu_selection == 5:
+        bucket_list = mydefs.load_buckets()
+        if len(bucket_list) == 0:
+            print("No buckets found.")
+            time.sleep(3)
+            mydefs.clear_console()
+            main_menu()
+        curcount = 0
+        for bucket in bucket_list:
+            print(curcount, " ", bucket)
+            curcount += 1
+
+        bucketName = input("Which bucket would you like to access?(Select Number): ")
+        bucketName = bucket_list[int(bucketName)]
+        bucket = mydefs.s3.Bucket(bucketName)
+        curcount = 0
+        for s3_files in bucket.objects.all():
+            print(curcount, s3_files.key)
+            mydefs.file_list.append(s3_files.key)
+            curcount += 1
+        objectkey = input("Which file would you like to delete?(Select a number): ")
+        mydefs.delete_object(bucketName, mydefs.file_list[int(objectkey)])
+        print("The file was deleted!")
+        time.sleep(1)
+        mydefs.clear_console()
+        main_menu()
+
+main_menu()
